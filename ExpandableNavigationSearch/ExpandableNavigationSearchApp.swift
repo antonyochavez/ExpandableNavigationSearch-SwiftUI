b6342da6733cc0c6363ce1f6164cbd4ac536de97
Antonio Chávez <jesus.chavez@suramexico.com>
2024-05-27 20:34:36 -0600

    Initial Commit

---
+++ ExpandableNavigationSearch/ExpandableNavigationSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandableNavigationSearchApp.swift
+//  ExpandableNavigationSearch
+//
+//  Created by H&S on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpandableNavigationSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
