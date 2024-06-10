cabd7bbf4b479453d998388469a94e5847e6a604
Antonio Chávez <jesus.chavez@suramexico.com>
2024-06-09 21:55:10 -0600

    first commit

---
+++ ExpandableNavigationSearch/Model/Tab.swift
@@ -0,0 +1,15 @@
+//
+//  Tab.swift
+//  ExpandableNavigationSearch
+//
+//  Created by H&S on 27/05/24.
+//
+
+import SwiftUI
+
+enum Tab: String, CaseIterable{
+    case all = "All"
+    case personal = "Personal"
+    case office = "Update"
+    case community = "Gaming"
+}
